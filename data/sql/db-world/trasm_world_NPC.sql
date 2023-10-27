3896b88f005e5dc09f1a7dd1b947b79458a05c7d
jshplayer <jshplayer@outlook.com>
2023-10-27 20:40:07 +1300

    Re-adding OceWar additions are discarding commits

--- data/sql/db-world/trasm_world_NPC.sql
+++ data/sql/db-world/trasm_world_NPC.sql
@@ -1,29 +1,10 @@
 SET
 @Entry = 190010,
-@Name = "Warpweaver";
+@Name = "Oceanic War Transmog";
 DELETE FROM `creature_template` WHERE `entry` = @Entry;
 
-INSERT INTO `creature_template` (`entry`, `modelid1`, `modelid2`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `scale`, `rank`, `dmgschool`, `baseattacktime`, `rangeattacktime`, `unit_class`, `unit_flags`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `AIName`, `MovementType`, `HoverHeight`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `flags_extra`, `ScriptName`) VALUES
-(@Entry, 19646, 0, @Name, "Transmogrifier", NULL, 0, 80, 80, 2, 35, 1, 1, 0, 0, 2000, 0, 1, 0, 7, 138936390, 0, 0, 0, '', 0, 1, 0, 0, 1, 0, 0, 'npc_transmogrifier');
-
-DELETE FROM `creature_template_locale` WHERE `entry` IN  (@Entry);
-INSERT INTO `creature_template_locale` (`entry`, `locale`, `Name`, `Title`) VALUES
-(@Entry, 'koKR', @Name, "변형기"),
-(@Entry, 'frFR', @Name, "Transmogrificateur"),
-(@Entry, 'deDE', @Name, "Transmogrifier"),
-(@Entry, 'zhCN', @Name, "变形者"),
-(@Entry, 'zhTW', @Name, "幻化大師"),
-(@Entry, 'esES', @Name, "Transfigurador"),
-(@Entry, 'esMX', @Name, "Transfigurador"),
-(@Entry, 'ruRU', @Name, "Трансмогрификатор");
-
-SET
-@Entry = 190011,
-@Name = "Ethereal Warpweaver";
-DELETE FROM `creature_template` WHERE `entry` = @Entry;
-
-INSERT INTO `creature_template` (`entry`, `modelid1`, `modelid2`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `scale`, `rank`, `dmgschool`, `baseattacktime`, `rangeattacktime`, `unit_class`, `unit_flags`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `AIName`, `MovementType`, `HoverHeight`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `flags_extra`, `ScriptName`) VALUES
-(@Entry, 19646, 0, @Name, "Transmogrifier", NULL, 0, 80, 80, 2, 35, 1, 1, 0, 0, 2000, 0, 1, 0, 7, 138936390, 0, 0, 0, '', 0, 1, 0, 0, 1, 0, 0, 'npc_transmogrifier');
+INSERT INTO `creature_template` (`entry`, `modelid1`, `modelid2`, `name`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `scale`, `rank`, `dmgschool`, `baseattacktime`, `rangeattacktime`, `unit_class`, `unit_flags`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `AIName`, `MovementType`, `HoverHeight`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `flags_extra`, `ScriptName`) VALUES
+(@Entry, 19646, 0, @Name, NULL, 0, 80, 80, 2, 35, 1, 1, 0, 0, 2000, 0, 1, 0, 7, 138936390, 0, 0, 0, '', 0, 1, 0, 0, 1, 0, 0, 'npc_transmogrifier');
 
 DELETE FROM `creature_template_locale` WHERE `entry` IN  (@Entry);
 INSERT INTO `creature_template_locale` (`entry`, `locale`, `Name`, `Title`) VALUES
